ad50e198527acc7f78e9906ef62c6efcda712fc0
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-02-06 14:50:52 -0500

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,69 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::hash::Hash;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-pub fn forward_distance<T: PartialEq + Eq + Clone>(trace: &[T]) -> Vec<usize> {
-    let mut registers: Vec<T> = Vec::with_capacity(trace.len());
-    let mut counter: Vec<usize> = Vec::with_capacity(trace.len());
-    let mut distances: Vec<usize> = Vec::with_capacity(trace.len());
-    let mut index: usize = 0;
-
-    for i in trace.len() - 1..=0 {
-        let element = &trace[i];
-
-        // Update counter
-        for j in 0..registers.len() {
-            counter[j] += 1;
-        }
-
-        // Update registers and distances
-        if !registers.contains(element) {
-            registers.push(element.clone());
-            counter.push(usize::MAX);
-        } else {
-            index = registers.iter().position(|x| *x == *element).unwrap();
-            distances.insert(index, counter[index]);
-            counter[index] = 0;
+pub fn forward_distance<T: PartialEq + Eq + Clone + Hash>(trace: &[T]) -> Vec<usize> {
+    let mut last_occurence = HashMap::new();
+    let mut result = vec![0; trace.len()];
+    for (i, element) in trace.iter().enumerate().rev() {
+        match last_occurence.entry(element) {
+            Entry::Occupied(mut x) => {
+                result[i] = x.get() - i;
+                *x.get_mut() = i;
+            }
+            Entry::Vacant(x) => {
+                result[i] = usize::MAX;
+                x.insert(i);
+            }
         }
     }
-
-    return distances;
+    result
 }
 
-pub fn evicted_by_rule_omega<T: PartialEq + Eq + Clone>(register1: T, register2: T) -> T {
-    todo!("Eviction policy not implemented");
-}
+// pub fn evicted_by_rule_omega<T: PartialEq + Eq + Clone>(register1: T, register2: T) -> T {
+//     todo!("Eviction policy not implemented");
+// }
 
-pub fn opt_miss_ratio<T: PartialEq + Eq + Clone>(trace: &[T], cache_size: usize) -> f64 {
+pub fn opt_miss_ratio<T: PartialEq + Eq + Clone + Hash>(trace: &[T], cache_size: usize) -> f64 {
     let mut cache_accesses: usize = 0;
     let mut cache_misses: usize = 0;
     let mut cache_registers: Vec<T> = Vec::with_capacity(cache_size);
     let mut cache_distances: Vec<usize> = Vec::with_capacity(cache_size);
-    let mut time: usize = 0;
-    let mut evict_index: usize = 0;
-
     let forward_distances = forward_distance(trace);
 
-    for element in trace {
+    for (time, element) in trace.iter().enumerate() {
         // Access cache
         cache_accesses += 1;
+
         if !cache_registers.contains(element) {
             cache_misses += 1;
             if cache_registers.len() == cache_size {
                 // Evict
-                evict_index = cache_distances
+                let evict_index = cache_distances
                     .iter()
                     .position(|x| x == cache_distances.iter().max().unwrap())
                     .unwrap();
-                cache_registers.remove(evict_index);
-                cache_distances.remove(evict_index);
+                cache_registers[evict_index] = element.clone();
+                cache_distances[evict_index] = forward_distances[time];
+            } else {
+                cache_registers.push(element.clone());
+                cache_distances.push(forward_distances[time]);
             }
-            cache_registers.push(element.clone());
-            cache_distances.push(forward_distances[time]);
         }
 
         // Update cache
-        for i in 0..cache_registers.len() {
-            cache_distances[i] -= 1;
+        for i in cache_distances.iter_mut() {
+            if *i == 0 {
+                *i = forward_distances[time];
+            }
+            *i -= 1;
         }
-
-        // Update time
-        time += 1;
     }
 
-    return cache_misses as f64 / cache_accesses as f64;
+    cache_misses as f64 / cache_accesses as f64
 }
 
 #[cfg(test)]
@@ -79,8 +71,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn forward_distance_test() {
+        let trace = vec![1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 5, 6, 5, 6];
+        let result = forward_distance(&trace);
+        assert_eq!(
+            result,
+            [
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                usize::MAX,
+                usize::MAX,
+                usize::MAX,
+                usize::MAX,
+                2,
+                2,
+                usize::MAX,
+                usize::MAX
+            ]
+        );
+    }
+
+    #[test]
+    fn opt_miss_ratio_test() {
+        let trace = vec![1, 213, 43, 1, 2, 3, 4, 213, 9, 5, 43, 1, 5, 32, 213];
+        // let result = opt_miss_ratio(&trace, 4);
+        // assert_eq!(result, 1.0 / 3.0);
+        // let result = opt_miss_ratio(&trace, 2);
+        // assert_eq!(result, 1.0 / 3.0);
+        let result = opt_miss_ratio(&trace, 100);
+        assert_eq!(result, 0.6);
     }
 }
